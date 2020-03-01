f35ad07764f2c0a0bb01e3007dfe31a25f3022e0
Antz <antonio.mtz.rz@gmail.com>
2020-03-01 13:07:13 -0600

    usuario

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const Usuario = require('../models/user')
 router.get('/users/singin', (req, res) => {
     res.render('./users/singin')
 });
@@ -9,7 +9,7 @@ router.get('/users/signup', (req, res) => {
     res.render('./users/singup')
 });
 
-router.post('/users/signup', (req, res) => {
+router.post('/users/signup', async (req, res) => {
     console.log(req.body);
     const { usuario, Nombre, password, password1 } = req.body;
     error_mesa = [];
@@ -35,22 +35,50 @@ router.post('/users/signup', (req, res) => {
         console.log("En el índice " + indice + " hay este valor: " + valor.text);
     });
 
-    const errors=[];
+    const errors = [];
     if (error_mesa.length > 0) {
         errors.push({ text: errores_dis });
         req.flash('error_ms', errores_dis);
-        res.render('users/singup',{
+        res.render('users/singup', {
             errors,
             usuario,
-            Nombre, 
-            password, 
+            Nombre,
+            password,
             password1
         })
     } else {
-        req.flash('success', "Los datos son correctos");
-        res.redirect('/notes');
+        try {
+            const newUser = new Usuario({ idUser: usuario, Password: password, Nombre: Nombre });
+            newUser.Password = await newUser.encrypPass(password);
+            await newUser.save();
+            req.flash('success', "Los datos son correctos");
+            res.redirect('/users/singin');
+        } catch (error) {
+            const errors = [];
+            if (error.code == '11000') {
+                req.flash('error_ms', "El usuario " + usuario + " ya existe favor de introducir otro");
+                errors.push({ text: "El usuario " + usuario + " ya existe favor de introducir otro" });
+                res.render('users/singup', {
+                    errors,
+                    usuario,
+                    Nombre,
+                    password,
+                    password1
+                })
+            } else {
+                req.flash('error_ms', "El error es: " + error.message);
+                res.redirect('/users/singin', {
+                    usuario,
+                    Nombre,
+                    password,
+                    password1
+                });
+            }
+
+        }
+
     }
-    
+
 
 });
 module.exports = router;
