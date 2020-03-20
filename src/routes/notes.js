6783a41827fb20142ee262d28e67d00217dcc4af
Antz <antonio.mtz.rz@gmail.com>
2020-03-20 00:38:49 -0600

    Date

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -5,28 +5,21 @@ var moment = require('moment');
 const {isLoggedIn, isNotLoggedIn}=require('../lib/aut');
 
 router.get('/notes/add',isLoggedIn, (req, res) => {
-    console.log("Pase");
-    console.log(fecha34);
-    fechas=JSON.stringify(fecha34[0].fecha);
+    
     try {
+        fechas=JSON.stringify(fecha34[0].fecha);
         res.render('notes/newnote',{
          fechas
         });
     } catch (error) {
-        req.flash('error_ms', "No estas Logeado Favor de Logearte")
-        res.redirect('/');
+        req.flash('error_ms', error.message)
+        res.redirect('/notes');
     }
 });
 
 router.post('/notes/newnote',isLoggedIn, async (req, res) => {
     try {
         const { titulo, descripcion } = req.body;
-        console.log("Paso3");
-        
-        console.log(JSON.stringify(fecha34));
-        var fech02=new Date(moment(fecha34[0].fecha).format('YYYY-MM-DD'));
-        console.log(fech02);
-        
         const errors = [];
         if (!titulo) {
             errors.push({ text: 'Porfavor escribe un titulo' });
@@ -41,12 +34,12 @@ router.post('/notes/newnote',isLoggedIn, async (req, res) => {
                 descripcion
             })
         } else {
-            
             const nuevaNota = new Nota({ titulo, descripcion });
             nuevaNota.user=req.user.IDMaestro;
-            nuevaNota.dia=new Date(moment(fecha34[0].fecha).format('YYYY-MM-DD'));
+
             //nuevaNota.dia=
             try {
+                nuevaNota.dia=new Date(moment(fecha34[0].fecha).format('YYYY-MM-DD'));
                 await nuevaNota.save();
                 req.flash('success', 'Se inserto el registro ' + titulo + ' de forma correcta');
             } catch (error) {
