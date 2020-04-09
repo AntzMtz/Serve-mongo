4a1b015b88ad1176629e7001eaed36540691ca6c
Antz <antonio.mtz.rz@gmail.com>
2020-04-08 22:42:16 -0500

    QrGener

--- src/routes/Alumnos.js
+++ src/routes/Alumnos.js
@@ -1,62 +1,121 @@
 const express = require('express');
 const router = express.Router();
-
 const path1 = require('path');
-
 const { isLoggedIn, isNotLoggedIn } = require('../lib/aut');
 const alumno = require('../models/alumno');
 const fs = require('fs');
 
+const qrcode = require('qrcode');
+
 router.get('/Alumnos/add', isLoggedIn, (req, res) => {
-    res.render('Alumno/alumnosAdd.hbs')
+
+    res.render('Alumno/alumnosAdd.hbs');
+
 });
 
 router.post('/Alumnos/add', isLoggedIn, async (req, res) => {
-    console.log(req.body);
     const { idAlumno, idEscuela, codQr, grupo, grado, correoTutor, nombreTutor, nombre, aPaterno, aMaterno, direccion, delegacion, estado } = req.body;
+    const errors1 = [];
+    const errors = [];
     try {
+
+        if (idAlumno == "") {
+            errors1.push({ text: 'Debes de Ingresar una clave de Usuario' });
+        }
+        if (idEscuela == "") {
+            errors1.push({ text: 'Debes de Ingresar la clave de la Escuela' });
+        }
+        if (codQr == "") {
+            errors1.push({ text: 'Generar el codigo QR del alumno' });
+        }
+        if (grupo == "") {
+            errors1.push({ text: 'Ingresar el grupo del Alumno' });
+        }
+        if (grado == "") {
+            errors1.push({ text: 'Ingresar el Grado del Alumno' });
+        }
+        if (correoTutor == "") {
+            errors1.push({ text: 'Ingresar el Email del Tutor Legal' });
+        }
+        if (nombreTutor == "") {
+            errors1.push({ text: 'Debes de Ingresar El nombre completo del Tutor' });
+        }
+        if (nombre == "") {
+            errors1.push({ text: 'Ingresar el Nombre del Alumno' });
+        }
+        if (aPaterno == "") {
+            errors1.push({ text: 'Ingresar el Apellido Paterno del Alumno' });
+        }
+        if (aMaterno == "") {
+            errors1.push({ text: 'Ingresar el Apellido Materno del Alumno' });
+        }
+        if (direccion == "") {
+            errors1.push({ text: 'Ingresar la Dirección del Alumno' });
+        }
+        if (delegacion == "") {
+            errors1.push({ text: 'Ingresar la Delegación' });
+        }
+        if (estado == "") {
+            errors1.push({ text: 'Ingresar el Estado' });
+        }
+
+
+
         const newAlum = new alumno({
             idAlumno, idEscuela, codQr, grado, grupo, correoTutor, nombreTutor, nombre, aPaterno, aMaterno, direccion, delegacion, estado
         });
-        console.log("Pase 3");
-
         await newAlum.save();
-        console.log("Pase 4");
+        const path3 = path1.join(__dirname, '../', 'imagu/');
+        var clave = codQr;
+        var nomb = idAlumno + idEscuela;
+        qrcode.toFile(path3 + nomb + '.png', clave, { width: 500 })
         req.flash('success', "Registro insertado de forma satisfactoria");
         res.redirect('/Alumnos/add')
 
     } catch (error) {
-        const errors = [];
+
         if (error.code == '11000') {
             errors.push({ text: "El Alumno " + idAlumno + " de la escuela " + idEscuela + " ya existe favor de introducir otro" });
         } else {
-            errors.push({ text: "El error es: " + error });
-            //req.flash('error', "El error es: " + error);
+            if (errors1.length > 0) {
+                var errores_dis = "";
+                errors1.forEach(function (valor, indice, array) {
+                    if (errors1.length <= indice + 1) {
+                        errores_dis += valor.text + " Favor de validar";
+                    } else {
+                        errores_dis += valor.text + " , ";
+                    }
+                });
+                errors.push({ text: errores_dis });
+            } else {
+                errors.push({ text: "El error es: " + error });
+            }
         }
-        console.log("Error: " + error);
 
-        res.render('Alumno/alumnosAdd.hbs', { idAlumno, idEscuela, codQr, grado, correoTutor, nombreTutor, nombre, aPaterno, aMaterno, direccion, delegacion, estado, errors })
+
+        console.log("Error: " + error);
+        res.render('Alumno/alumnosAdd.hbs', { idAlumno, idEscuela, codQr, grupo, grado, correoTutor, nombreTutor, nombre, aPaterno, aMaterno, direccion, delegacion, estado, errors })
     }
 
 });
 
 
-router.get('/Alumnos/imagenes', (req, res) => {
+router.get('/Alumnos/imagenes', isLoggedIn, (req, res) => {
     res.render('Alumno/imagenes.hbs')
 });
 
-router.post('/Alumnos/imagenes', (req, res) => {
+router.post('/Alumnos/imagenes', isLoggedIn, (req, res) => {
 
-    const path3=path1.join(__dirname,'../','imagu/');
-    const { path, name } = req.files.archivo; 
-    fs.rename(path, path3 + name, () => { 
-        null 
-      }); 
+    const path3 = path1.join(__dirname, '../', 'imagu/');
+    const { path, name } = req.files.archivo;
+    fs.rename(path, path3 + name, () => {
+        null
+    });
     res.send(path3 + name)
 
 
 });
- 
+
 
 
 module.exports = router;
